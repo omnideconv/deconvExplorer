3aa28b5ebfb5975ce98fc696ffc7e5de508af010
Constantin Zackl <constantin.zackl@gmail.com>
2021-11-02 16:24:42 +0100

    added box for selection of deconvolution results

--- app.R
+++ app.R
@@ -33,7 +33,7 @@ deconvExplorer = function(usr_bulk = NULL, usr_singleCell = NULL, usr_cellAnnota
                      selectInput("deconvMethod", "Deconvolution Method", choices = omnideconv::deconvolution_methods),
                      conditionalPanel(
                        condition = "input.deconvMethod == 'bisque'|| input.deconvMethod == 'cibersortx' || input.deconvMethod == 'dwls' || input.deconvMethod == 'momf'",
-                       selectInput("sigMethod", "Signature Calculation Method", choices = methods_reduced)
+                       selectInput("signatureMethod", "Signature Calculation Method", choices = methods_reduced)
                      ),
                      conditionalPanel(
                        condition = "input.deconvMethod == 'bseqsc'",
@@ -49,6 +49,14 @@ deconvExplorer = function(usr_bulk = NULL, usr_singleCell = NULL, usr_cellAnnota
   deconv_table_box = box(title="Deconvolution Table", status = "warning", solidHeader = TRUE, width = 12, 
                          DT::dataTableOutput("tableBox") %>%  withSpinner())
   
+  deconv_signature_box = box(title="Deconvolution Signature", status = "info", solidHeader = TRUE, width = 12, 
+                         downloadButton("signatureDownload", "Download Signature"),
+                         DT::dataTableOutput("signatureBox") %>% withSpinner())
+  deconv_all_results = box(title = "All Deconvolutions", status = "info", solidHeader = TRUE, width = 12, 
+                           selectInput ("computedDeconvMethod", "Deconvolution Method", choices = NULL), 
+                           selectInput ("computedSignatureMethod", "Signature Method", choices = NULL), 
+                           actionButton("loadDeconvolution", "Load Deconvolution Result"))
+  
 
   # ui definition  ----------------------------------------------------------
 
@@ -57,10 +65,12 @@ deconvExplorer = function(usr_bulk = NULL, usr_singleCell = NULL, usr_cellAnnota
     dashboardHeader(title = "Omnideconv"),
     dashboardSidebar(sidebarMenu(
       menuItem("Deconvolution", tabName = "deconv"), 
+      menuItem("Benchmarking", tabName = "benchmark"),
       menuItem("Further Information", tabName= "fInfo")
     )),
     dashboardBody(tabItems(
-      tabItem(tabName="deconv", fluidPage(fluidRow(data_upload_box, settings_box), fluidRow(deconv_plot_box, deconv_table_box))),
+      tabItem(tabName="deconv", fluidPage(fluidRow(deconv_all_results), fluidRow(data_upload_box, settings_box), fluidRow(deconv_plot_box, deconv_table_box, deconv_signature_box))),
+      tabItem(tabName="benchmark", fluidPage(textOutput("test"))), 
       tabItem(tabName="fInfo", fluidPage(includeMarkdown("omnideconv_vignette.md")))
     ))
   )
@@ -69,11 +79,47 @@ deconvExplorer = function(usr_bulk = NULL, usr_singleCell = NULL, usr_cellAnnota
   
   # server definition  ------------------------------------------------------
   
-  server = shinyServer(function(input, output) {
-   
+  server = shinyServer(function(input, output, session) {
+    ### background datastructure to store several 
+    
+    # idea: just use a reactive value, access: all_deconvolutions[[deconvMethod_signatureMethod]] = c(DeconvResult, Signature)
+    # only storing the results! Not the input files
+    # vectors must be the same data structure! should be a problem as both are double matrices 
+    # indexing starts at 1 
+    
+    
+    
+    all_deconvolutions = reactiveValues()
+    
+    
+    
+    
+    # methods= unname(omnideconv::deconvolution_methods)
+    # 
+    # df = data.frame(matrix(nrow = length(methods), ncol = length (methods)))
+    # rownames(df) = methods
+    # colnames(df) = methods
+    
+    # for later: run all possible combinations! i and j cover all valid
+    # deconvolution / signature combinations 
+    
+    # for (i in methods){
+    #   if (i %in% methods_interchangeable){
+    #     for (j in methods_reduced){
+    #       df[j, i] = "X" # insert as df[signature, deconvMethod]
+    #     }
+    #   }
+    #   df[i, i] = "X" # todo: skip the ones already inserted in for loop above
+    # }
+    # 
+    
+    
+    
+    
+    
     waitress = Waitress$new("#deconvolute", infinite=TRUE)
     
-    values = reactiveValues() # storing everything
+    values = reactiveValues() # storing the current deconvolution 
     
     ### ersetzten mit dem laden der user Uploads!
     values$single_cell = omnideconv::single_cell_data_1
@@ -85,12 +131,10 @@ deconvExplorer = function(usr_bulk = NULL, usr_singleCell = NULL, usr_cellAnnota
     
     
     
-    
-    
     signature = reactive(omnideconv::build_model(
       single_cell_object = values$single_cell, 
       cell_type_annotations = values$cell_annotations, 
-      method = input$sigMethod, 
+      method = input$signatureMethod, 
       batch_ids = values$batch_ids, 
       bulk_gene_expression = values$bulk
     ))
@@ -100,32 +144,75 @@ deconvExplorer = function(usr_bulk = NULL, usr_singleCell = NULL, usr_cellAnnota
     # deconvolute when button is clicked
     observeEvent(input$deconvolute, {
       waitress$start()
-      signature_Method = input$sigMethod
+      signature_Method = input$signatureMethod
       if (!(input$deconvMethod %in% methods_interchangeable)){
         signature_Method = input$deconvMethod
+        
       }
       message(paste0("Starting Deconvolution. Deconvolution: ", input$deconvMethod, ", Signature: ", signature_Method))
+     
+      # save reactive siganture to values
+      values$signature = signature()
       
       #### At this Point the correct Variables for the Deconvolution are: 
       #### Deconvolution Method: input$deconvMethod
       #### Signature Calculation Method: signature_Method
-      
+      #### Signature: values$signature
       
       values$deconvolution_result =
         omnideconv::deconvolute(
           bulk_gene_expression = values$bulk,
-          signature = signature(),
+          signature = values$signature,
           method = input$deconvMethod,
           single_cell_object = values$single_cell,
           cell_type_annotations = values$cell_annotations,
           batch_ids = values$batch_ids
         )
+      
+      # insert result into the all_deconvolutions reactive Value
+      all_deconvolutions[[paste0(input$deconvMethod, "_", signature_Method)]] = list(values$deconvolution_result, values$signature)
+      
+      # update select input for selection, just the deconv Method
+      deconv_choices = names(all_deconvolutions) %>%
+                       strsplit("_") %>%
+                       unlist()
+      deconv_choices = deconv_choices[seq(1, length(deconv_choices), 2)] # jede 2, startend von 1
+      updateSelectInput(session, inputId = "computedDeconvMethod" , choices = deconv_choices)
+
       waitress$close()
       }
     )
     
+    # load Deconvolution result
+    observeEvent(input$loadDeconvolution, {
+      result = all_deconvolutions[[paste0(input$computedDeconvMethod, "_", input$computedSignatureMethod)]]
+      message(typeof(result))
+      # values$deconvolution_result = result[1]
+      # values$signature = result[2]
+    })
+    
+    # update signature Method choices when selecting deconvolution to load 
+    observe({
+      signature_choices = names(all_deconvolutions) %>%
+        stringr::str_subset(pattern = paste0(input$computedDeconvMethod, "_"))  %>%
+        strsplit("_") %>%
+        unlist()
+      
+      if (length(signature_choices)>1){
+        signature_choices = signature_choices[seq(2, length(signature_choices), 2)] # jede zweite ab dem zweiten 
+      } else {
+        signature_choices = signature_choices[2] # nur das zweite
+      }
+
+      updateSelectInput(session, inputId = "computedSignatureMethod", choices = signature_choices)
+    })
+    
 
     # Plots -------------------------------------------------------------------
+    
+    output$test = renderText({
+      names(all_deconvolutions)
+    })
     
     output$plotBox  = plotly::renderPlotly({
       data = cbind(values$deconvolution_result, samples = rownames(values$deconvolution_result)) %>%
@@ -188,7 +275,22 @@ deconvExplorer = function(usr_bulk = NULL, usr_singleCell = NULL, usr_cellAnnota
                         DT::formatPercentage(c("B", "CD4 T", "CD8 T", "DC", "Mono", "NK"), 2)
                       
       })
-   
+    
+    output$signatureBox = DT::renderDataTable({
+                          DT::datatable(values$signature)# %>%
+                         # DT::formatRound(c("B", "CD4 T", "CD8 T", "DC", "Mono", "NK"), 2)
+      })
+    
+    
+    output$signatureDownload = downloadHandler(
+                              filename = function(){
+                                paste("signature", ".csv", sep="")
+                              }, 
+                              content = function(file){
+                                write.csv(values$signature, file)
+                              }
+    )
+    
   })
   
   shiny::shinyApp(ui = ui, server = server)
