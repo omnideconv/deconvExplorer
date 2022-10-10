3825375ccff176f5cf9c27d94f6f188f8173570a
Federico Marini <marinif@uni-mainz.de>
2022-10-10 16:09:20 +0200

    activating tests for signature refinement functions

--- tests/testthat/test-signatures.R
+++ tests/testthat/test-signatures.R
@@ -8,17 +8,21 @@ test_that("Signature refinement operations are correct", {
   expect_true("B" %in% colnames(signature_list$bisque))
   expect_true("B.cells" %in% colnames(signature_renamed))
 
-  # COSTODO: will need to put away the notifications
-  # signature_nozeros <- removePercentZeros(baseSignature = signature_list$bisque,
-  #                                         percentage = 0.5)
-  #
-  # signature_nounspecific <- removeUnspecificGenes(signature = signature_list$momf,
-  #                                                 numberOfBins = 3,
-  #                                                 maxCount = 1)
-  #
-  # signature_selgenes <- selectGenesByScore(signature = signature_list,
-  #                                          method = "gini",
-  #                                          genesPerCellType = 50)
+  
+
+  signature_nozeros <- removePercentZeros(baseSignature = signature_list$bisque,
+                                          percentage = 0.5)
+  expect_equal(nrow(signature_list$bisque) - nrow(signature_nozeros), 8)
+  
+  signature_nounspecific <- removeUnspecificGenes(signature = signature_list$momf,
+                                                  numberOfBins = 3,
+                                                  maxCount = 1)
+  expect_equal(nrow(signature_list$momf) - nrow(signature_nounspecific), 199)
+  
+  signature_selgenes <- selectGenesByScore(signature = signature_list$bisque,
+                                           method = "gini",
+                                           genesPerCellType = 50)
+  expect_equal(nrow(signature_list$bisque) - nrow(signature_selgenes), 126)
 
   entropy_calc <- scoreEntropy(signature_list$bisque[1, ])
   expect_true(is.numeric(entropy_calc))
