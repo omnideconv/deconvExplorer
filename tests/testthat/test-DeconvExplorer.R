d420115233a2841b1b59e19ca001a8018bf33d69
Federico Marini <marinif@uni-mainz.de>
2022-12-19 21:13:09 +0100

    cleanup double test

--- tests/testthat/test-DeconvExplorer.R
+++ tests/testthat/test-DeconvExplorer.R
@@ -2,10 +2,6 @@ test_that("Shiny app is generated", {
   expect_true(
     is(DeconvExplorer::DeconvExplorer(), "shiny.appobj")
   )
-
-  expect_true(
-    is(DeconvExplorer::DeconvExplorer(), "shiny.appobj")
-  )
 })
 
 test_that("Helper funcs", {
