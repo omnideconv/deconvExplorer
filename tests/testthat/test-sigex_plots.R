0bc228b493a7c17e1ba3d460774456eea9d30eef
Federico Marini <marinif@uni-mainz.de>
2022-12-20 15:40:32 +0100

    score -> scoring_method

--- tests/testthat/test-sigex_plots.R
+++ tests/testthat/test-sigex_plots.R
@@ -21,40 +21,40 @@ test_that("Signature exploration plots are correct", {
 
   p_sig_clust <- plot_signatureClustered(
     signature_mat = signature_list$bisque,
-    score = "gini", annotation_type = "bar"
+    scoring_method = "gini", annotation_type = "bar"
   )
 
   expect_s4_class(p_sig_clust, "HeatmapList")
   expect_error(
     plot_signatureClustered(
       signature_mat = signature_list,
-      score = "gini", annotation_type = "bar"
+      scoring_method = "gini", annotation_type = "bar"
     )
   )
   expect_error(
     plot_signatureClustered(
       signature_mat = signature_list,
-      score = "gino", annotation_type = "bar"
+      scoring_method = "gino", annotation_type = "bar"
     )
   )
   expect_error(
     plot_signatureClustered(
       signature_mat = signature_list,
-      score = "gini", annotation_type = "bart"
+      scoring_method = "gini", annotation_type = "bart"
     )
   )
   # some variations on that
   p_sig_clust2 <- plot_signatureClustered(
     signature_mat = signature_list$bisque,
-    score = "gini", annotation_type = "line"
+    scoring_method = "gini", annotation_type = "line"
   )
   p_sig_clust3 <- plot_signatureClustered(
     signature_mat = signature_list$bisque,
-    score = "entropy", annotation_type = "bar"
+    scoring_method = "entropy", annotation_type = "bar"
   )
   p_sig_clust4 <- plot_signatureClustered(
     signature_mat = signature_list$bisque,
-    score = "entropy", annotation_type = "bar"
+    scoring_method = "entropy", annotation_type = "bar"
   )
 
   expect_s4_class(p_sig_clust2, "HeatmapList")
