11c5997a3d5820ab5b093dd5d1aa0d3f6d82bbf5
Federico Marini <marinif@uni-mainz.de>
2023-02-20 22:28:23 +0100

    namespace updated

--- R/DeconvExplorer-pkg.R
+++ R/DeconvExplorer-pkg.R
@@ -29,7 +29,7 @@
 #' @importFrom circlize colorRamp2
 #' @importFrom ggforce facet_grid_paginate
 #' @importFrom grDevices colorRampPalette
-#' @importFrom ggpubr rotate_x_text stat_cor
+#' @importFrom ggpubr stat_cor
 #' @importFrom corrplot corrplot
 #' @importFrom SummarizedExperiment assays
 #'
